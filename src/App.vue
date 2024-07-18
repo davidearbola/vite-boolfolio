02165053ad7cf9e7bbc02d32c0d54248a1b02fc1
davidearbola <davidearbola@gmail.com>
2024-07-18 16:13:59 +0200

    milestone 4-5 creo nuovo progetto con vite e con chiamata axios stampo in App.vue le card dei projects recuperati usando un component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from "axios";
+import ProjectCard from "./components/ProjectCard.vue";
+
+export default {
+	name: "App",
+	components: {
+		ProjectCard,
+	},
+	data() {
+		return {
+			base_url: "http://127.0.0.1:8000/",
+			projects: [],
+		};
+	},
+	methods: {
+		getProjectList() {
+			axios.get(`${this.base_url}api/projects`).then((result) => {
+				this.projects = result.data.projects;
+				console.log(this.projects);
+			});
+		},
+	},
+	mounted() {
+		this.getProjectList();
+	},
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<div class="d-flex flex-wrap">
+		<ProjectCard :elemento="projects.data" :url="base_url" />
+	</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+	name: "ProjectCard",
+	props: ["elemento", "url"],
+};
+</script>
+
+<template>
+	<div v-for="project in elemento" class="card col-4">
+		<template v-if="!project.thumb_path.startsWith('http')">
+			<img class="card-img-top" :src="url + 'storage/' + project.thumb_path" />
+		</template>
+		<template v-else>
+			<img class="card-img-top" :src="project.thumb_path" />
+		</template>
+		<div class="card-body">
+			<h4 class="card-title">{{ project.name }}</h4>
+			<p class="card-text">{{ project.description }}</p>
+		</div>
+	</div>
+</template>
+
+<style scoped></style>
