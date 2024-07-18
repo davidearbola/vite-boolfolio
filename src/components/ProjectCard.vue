165da22d96358b02ad692aea59fc364128d4a36d
davidearbola <davidearbola@gmail.com>
2024-07-18 16:22:26 +0200

    aggiunto campi per linguaggi e tipi nelle card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -16,6 +16,10 @@ export default {
 		<div class="card-body">
 			<h4 class="card-title">{{ project.name }}</h4>
 			<p class="card-text">{{ project.description }}</p>
+			<p>Linguaggi Utilizzati:</p>
+			<i v-for="language in project.languages" :class="language.icon" class="me-2"></i>
+			<p>Tipo di progetto:</p>
+			<p>{{ project.type.name }}</p>
 		</div>
 	</div>
 </template>
