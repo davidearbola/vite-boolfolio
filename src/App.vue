ab25f7d39f7818e01fffd334b4c84de27346fca0
davidearbola <davidearbola@gmail.com>
2024-07-18 18:58:31 +0200

    aggiunta pagination bonus

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,11 @@ export default {
 		getProjectList() {
 			axios.get(`${this.base_url}api/projects`).then((result) => {
 				this.projects = result.data.projects;
-				console.log(this.projects);
+			});
+		},
+		changePage(url) {
+			axios.get(url).then((result) => {
+				this.projects = result.data.projects;
 			});
 		},
 	},
@@ -29,7 +33,16 @@ export default {
 
 <template>
 	<div class="d-flex flex-wrap">
-		<ProjectCard :elemento="projects.data" :url="base_url" />
+		<div v-for="project in projects.data" class="card col-4">
+			<ProjectCard :project="project" :url="base_url" />
+		</div>
+		<nav aria-label="Page navigation">
+			<ul class="pagination">
+				<li v-for="link in projects.links" class="page-item" :class="{ disabled: !link.url, active: link.active }">
+					<a href="#" class="page-link" @click="changePage(link.url)" v-html="link.label"></a>
+				</li>
+			</ul>
+		</nav>
 	</div>
 </template>
 
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,26 +1,24 @@
 <script>
 export default {
 	name: "ProjectCard",
-	props: ["elemento", "url"],
+	props: ["project", "url"],
 };
 </script>
 
 <template>
-	<div v-for="project in elemento" class="card col-4">
-		<template v-if="!project.thumb_path.startsWith('http')">
-			<img class="card-img-top" :src="url + 'storage/' + project.thumb_path" />
-		</template>
-		<template v-else>
-			<img class="card-img-top" :src="project.thumb_path" />
-		</template>
-		<div class="card-body">
-			<h4 class="card-title">{{ project.name }}</h4>
-			<p class="card-text">{{ project.description }}</p>
-			<p>Linguaggi Utilizzati:</p>
-			<i v-for="language in project.languages" :class="language.icon" class="me-2"></i>
-			<p>Tipo di progetto:</p>
-			<p>{{ project.type.name }}</p>
-		</div>
+	<template v-if="!project.thumb_path.startsWith('http')">
+		<img class="card-img-top" :src="url + 'storage/' + project.thumb_path" />
+	</template>
+	<template v-else>
+		<img class="card-img-top" :src="project.thumb_path" />
+	</template>
+	<div class="card-body">
+		<h4 class="card-title">{{ project.name }}</h4>
+		<p class="card-text">{{ project.description }}</p>
+		<p>Linguaggi Utilizzati:</p>
+		<i v-for="language in project.languages" :class="language.icon" class="me-2"></i>
+		<p>Tipo di progetto:</p>
+		<p>{{ project.type.name }}</p>
 	</div>
 </template>
 
