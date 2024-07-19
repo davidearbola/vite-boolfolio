779ed7e91aa1d14c96ebbfc6521854107306087f
davidearbola <davidearbola@gmail.com>
2024-07-19 14:18:25 +0200

    welcome message in AppHome

---
+++ src/components/views/AppHome.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+	name: "AppHome",
+	components: {},
+	data() {
+		return {};
+	},
+	methods: {},
+	mounted() {},
+};
+</script>
+
+<template>
+	<h1>Benvenuto nella HomePage</h1>
+</template>
+
+<style scoped></style>
