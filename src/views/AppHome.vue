c61e70429d23f067cb2f03fa49644fcc6f012628
davidearbola <davidearbola@gmail.com>
2024-07-22 14:41:45 +0200

    add latest projects in homepage

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,17 +1,33 @@
 <script>
+import axios from "axios";
+import Store from "../data/Store";
+import ProjectCard from "../components/elements/ProjectCard.vue";
 export default {
 	name: "AppHome",
-	components: {},
+	components: {
+		ProjectCard,
+	},
 	data() {
-		return {};
+		return {
+			Store,
+			latestProjects: null,
+		};
 	},
 	methods: {},
-	mounted() {},
+	mounted() {
+		axios.get(`${Store.base_url}api/projects/latest`).then((response) => {
+			this.latestProjects = response.data.projects;
+		});
+	},
 };
 </script>
 
 <template>
-	<h1>Benvenuto nella HomePage</h1>
+	<div v-if="latestProjects" class="d-flex flex-wrap">
+		<div v-for="project in latestProjects" class="card col-4">
+			<ProjectCard :project="project" :url="Store.base_url" />
+		</div>
+	</div>
 </template>
 
 <style scoped></style>
