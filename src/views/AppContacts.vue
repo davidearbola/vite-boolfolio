54a68de8109c1a1da6853da6de493d9a4a04ceb8
davidearbola <davidearbola@gmail.com>
2024-07-23 14:50:47 +0200

    add contact form

--- src/views/AppContacts.vue
+++ src/views/AppContacts.vue
@@ -11,7 +11,23 @@ export default {
 </script>
 
 <template>
-	<h1>Benvenuto nella ContactsPage</h1>
+	<form action="">
+		<div class="mb-3">
+			<label for="nameForm" class="form-label">Name</label>
+			<input type="text" class="form-control" id="nameForm" />
+		</div>
+		<div class="mb-3">
+			<label for="emailForm" class="form-label">Email address</label>
+			<input type="email" class="form-control" id="emailForm" />
+		</div>
+		<div class="mb-3">
+			<label for="messageForm" class="form-label">Message</label>
+			<textarea class="form-control" id="messageForm" rows="3"></textarea>
+		</div>
+		<div>
+			<button class="btn btn-dark" type="submit">Submit</button>
+		</div>
+	</form>
 </template>
 
 <style scoped></style>
